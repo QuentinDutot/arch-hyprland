9050173077da7a54fcba0f6bc3fe56f2cfca349a
Quentin Dutot <dutot.quentin@gmail.com>
2023-09-08 01:12:26 +0200

    updater to purge cache and check reboot need

--- .config/waybar/scripts/updater.sh
+++ .config/waybar/scripts/updater.sh
@@ -2,17 +2,14 @@
 
 # https://github.com/speltriao/Pacman-Update-for-GNOME-Shell
 
-# Calculate updates for each service
 AUR=$(yay -Qua | wc -l)
 OFFICIAL=$(checkupdates | wc -l)
 
-# Case/switch for each service updates
 case $1 in
     aur) echo "$AUR updates";;
     official) echo "$OFFICIAL updates";;
 esac
 
-# If the parameter is "check", return the count of updates
 if [ "$1" = "check" ]; then
     # Calculate total number of updates
     COUNT=$((OFFICIAL+AUR))
@@ -27,8 +24,20 @@ if [ "$1" = "check" ]; then
     exit 0
 fi
 
-# If the parameter is "update", update all services
 if [ "$1" = "update" ]; then
     kitty sh -c 'yay -Syu'
     notify-send "System updated"
+
+    # Purge yay/pacman old caches and orphans
+    yay -Sc
+    sudo pacman -Rns $(pacman -Qdtq)
+    sudo paccache -r
+    notify-send "System cleaned"
+
+    # Check if a reboot might be required
+    kernel_updated=[[ $(uname -r) != $(file /boot/vmlinuz-linux | cut -d ' ' -f 5-6) ]]
+    systemd_updated=[ -f /usr/lib/systemd/systemd ] && [ /usr/lib/systemd/systemd -nt /proc/1/exe ]
+    if $kernel_updated || $systemd_updated; then
+        notify-send "Reboot needed"
+    fi
 fi
